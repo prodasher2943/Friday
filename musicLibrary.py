00718ce224e982c0de76abf1ee268b68dda2915e
prodasher2943 <prodasher2943@gmail.com>
2025-07-09 01:39:43 +0530

    Create musicLibrary.py

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "old songs" : "https://www.youtube.com/watch?v=F_KRgQTM_Sw&list=RDMM&start_radio=1",
+    "song" : "https://www.youtube.com/watch?v=oz5Li-ba--w&list=PL3V4g9RDsoj5VhXDJPXNaC7UlrNn43pBx",
+    "shaky" : "https://www.youtube.com/watch?v=sUf2PtEZris&list=RDsUf2PtEZris&start_radio=1",
+    "skyfall" : "https://www.youtube.com/watch?v=DuH3VpwRWDM&list=RDDuH3VpwRWDM"
+}
