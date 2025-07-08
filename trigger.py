9b2df1ccc2bf6df6d2d07e6d5343488284dc3d6d
prodasher2943 <prodasher2943@gmail.com>
2025-07-09 01:39:11 +0530

    Create trigger.py

---
+++ trigger.py
@@ -0,0 +1,19 @@
+trigger_word = {
+    "https://www.youtube.com/shorts/" : ["youtube shorts"],
+    "https://www.youtube.com/" : ["youtube"],
+    "https://www.nexttoppers.com/view-courses/details/11th_Class:28609&parent=" : ["my", "next toppers", "course"],
+    "https://www.nexttoppers.com/" : ["next toppers"],
+    "https://www.flipkart.com" : ["flipkart"],
+    "https://www.amazon.in" : ["amazon"],
+    "https://web.whatsapp.com/" : ["whatsapp"],
+    "https://hianime.sx/home" : ["anime"],
+    "https://instagram.com/" : ["instagram"],
+    "https://" : ["chrome"],
+    "https://mail.google.com/mail/u/0/#inbox" : ["mail"],
+    "https://footem.site" : ["football"],
+    "https://hotstar.com" : ["hotstar"],
+    "https://www.aternos.org/server/" : ["aternos"],
+    "https://open.spotify.com" : ["spotify"],
+    "https://chat.deepseek.com/" : ["deep seek"],
+    "https://www.meesho.com/" : ["meesho"]
+}
